b19eea179c7e94a026ed94368ba8f33da7cc0075
omarkhalil117 <omarkhalil117@gmail.com>
2024-01-04 06:45:14 +0200

    Lab[4]

--- Lab[4]/MyExceptions/MyClass.java
+++
@@ -1,9 +0,0 @@
-public class MyClass  {
-
-    public void division(int a , int b) throws MyException
-    {   
-        
-        throw new MyException("Division by zero");
-        
-    }
-}
--- Lab[4]/MyExceptions/MyException.java
+++
@@ -1,8 +0,0 @@
-public class MyException extends Exception {
-
-    public MyException(String s)
-    {
-        super(s);
-        System.out.println("My custom exception of Arthimetic Exception");
-    }
-}
